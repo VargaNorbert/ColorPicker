a95553dbd2fc1b1b6765240b01ff8709b57bfaa4
Varga Norbert <vnorbi02@gmail.com>
2022-09-19 11:30:49 +0200

    init

--- app/src/main/java/com/example/colorpicker/MainActivity.java
+++ app/src/main/java/com/example/colorpicker/MainActivity.java
@@ -8,17 +8,28 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    SeekBar s_red,s_green,s_blue;
-    TextView red,green,blue,color;
+    private SeekBar s_red,s_green,s_blue;
+    private TextView red,green,blue,color;
 
-    int piros=0,zold=0,kek=0;
-
-    
-
+    private int piros=0,zold=0,kek=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init(){
+
+        s_red=findViewById(R.id.s_red);
+        s_green=findViewById(R.id.s_green);
+        s_blue=findViewById(R.id.s_blue);
+
+        red= findViewById(R.id.red);
+        green= findViewById(R.id.green);
+        blue= findViewById(R.id.blue);
+
     }
 }
